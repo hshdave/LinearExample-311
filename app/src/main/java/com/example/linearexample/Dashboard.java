1533ba04dae56ce4cc72aea59f25a0c782caecb8
hdave <hshdave2011>
2019-06-12 11:32:30 -0400

    Intent Method First

--- app/src/main/java/com/example/linearexample/Dashboard.java
+++ app/src/main/java/com/example/linearexample/Dashboard.java
@@ -1,13 +1,33 @@
 package com.example.linearexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
+
+
+    String unm,upass;
+    TextView txt_nm,txt_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Intent i = getIntent();
+
+        unm = i.getStringExtra("un");
+        upass = i.getStringExtra("ps");
+
+        txt_nm = findViewById(R.id.txt_uname);
+        txt_pass = findViewById(R.id.txt_pass);
+
+
+        txt_nm.setText(unm);
+        txt_pass.setText(upass);
+
+
     }
 }
